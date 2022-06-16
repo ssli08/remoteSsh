6f7382a1efdde94ab6ead4866bfc844fe9a92e96
kevin <kevin@kevintekiMacBook-ea.local>
2022-06-16 15:56:10 +1200

    fix that ssh-keygen cmd line execution does't work when unique ip used another instance

--- modules/config.go
+++ modules/config.go
@@ -63,7 +63,8 @@ func InitSSHClientConfig(user, password, privateKey, proj string, timeout int) s
 			if errors.As(err, &keyErr) && len(keyErr.Want) > 0 {
 				// log.Printf("WARNING: %v is not a key of %s, either a MiTM attack or %s has reconfigured the host pub key.\n", hostKeyString(pubKey), host, host)
 				fmt.Printf("\n@@@@@@ WARNING: REMOTE HOST [%s] IDENTIFICATION HAS CHANGED! @@@@@\n", host)
-				fmt.Printf("remove %s with:\n\t ssh-keygen -f '%s' -R '%s' \n", host, knownHostFile, strings.Split(host, ":")[0])
+				// ssh-keygen -f "/home/rjyao/.ssh/known_hosts" -R "[54.202.245.21]:26222"
+				fmt.Printf("remove %s with:\n\t ssh-keygen -f '%s' -R '[%s]:%s' \n", host, knownHostFile, strings.Split(host, ":")[0], strings.Split(host, ":")[1])
 				return keyErr
 			} else if errors.As(err, &keyErr) && len(keyErr.Want) == 0 {
 				log.Printf("WARNING: %s is not trusted, adding this key: %q to known_hosts file.", host, hostKeyString(pubKey))
